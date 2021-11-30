fae8f2af7366d9724a5ee16e303f2c6300c973dc
villalbaflores3 <55265658+VillalbaF@users.noreply.github.com>
2021-11-29 21:20:56 -0600

    Prueba IS Villalba

---
+++ NetBeansProjects/PruebaGitHubVillalbaCurso/src/prueba/PruebaEstres1.java
@@ -0,0 +1,18 @@
+
+package prueba;
+
+/**
+ *
+ * @author Felipe
+ */
+public class PruebaEstres1 {
+
+    public static void main(String[] args) {
+        
+        for (int i = 0; i < 10000; i++) {
+            
+            System.out.println("Prueba Estres: " + i);
+        }
+    }
+    
+}
